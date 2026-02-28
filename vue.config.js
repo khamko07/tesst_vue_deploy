34e072819a64279e8507663b524fe1449c6505f1
Khamko <khamko@pascaliaasia.com>
2026-02-28 08:36:31 +0700

    feat: add deploy script and update publicPath for gh-pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/tesst_vue_deploy/'
 })
